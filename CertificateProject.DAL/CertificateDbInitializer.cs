e60fb1c6a3c77ac4a73a32eb563a0ecf65893541
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-04 21:21:38 +0200

    chore: initialize database

---
+++ CertificateProject.DAL/CertificateDbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CertificateProject.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CertificateProject.Infrastructure
+{
+    public class CertificateDbInitializer
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Certificate>().HasData(
+                new Certificate
+                {
+                    Id = 1,
+                    SequenceNumber = 1110,
+                    IssueDate = new DateTime(2020, 01, 20)
+                },
+                new Certificate
+                {
+                    Id = 2,
+                    SequenceNumber = 1111,
+                    IssueDate = new DateTime(2020, 03, 01)
+                }
+            );
+        }
+    }
+}
