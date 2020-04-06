7096f207e48919506b9408666818be16a66665ea
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-06 09:00:14 +0200

    chore: add AutoMapper mappings

---
+++ CertificateProject.Web/Mappings/AutoMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CertificateProject.Core.Domain;
+
+namespace CertificateProject.Web.Mappings
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<Certificate, CertificateEntity>();
+            CreateMap<CertificateEntity, Certificate>();
+        }
+    }
+}
