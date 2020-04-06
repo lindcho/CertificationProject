6e27ef9f593907ce18a7f940b62bd683f10f4699
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-06 16:21:27 +0200

    chore: comment out tests

--- CertificateProject.Test/CertificateRepositoryTest.cs
+++ CertificateProject.Test/CertificateRepositoryTest.cs
@@ -12,48 +12,49 @@ namespace CertificateProject.Test
     [Category("Acceptance")]
     public class CertificateRepositoryTest
     {
-        private CertificateRepository _repo;
+        //private CertificateRepository _repo;
 
-        [SetUp]
-        public void TestSetup()
-        {
-            var optionsBuilder = BuildDbConnection();
-            _repo = new CertificateRepository(new CertificateContext(optionsBuilder.Options));
-        }
+        //[SetUp]
+        //public void TestSetup()
+        //{
+        //    var optionsBuilder = BuildDbConnection();
+        //    var certificateContext = new CertificateContext(optionsBuilder.Options);
+        //    _repo = new CertificateRepository(certificateContext);
+        //}
 
-        private static DbContextOptionsBuilder<CertificateContext> BuildDbConnection()
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<CertificateContext>();
-            optionsBuilder.UseSqlServer(
-                "Server=(localdb)\\mssqllocaldb;Database=Certification;Trusted_Connection=True;MultipleActiveResultSets=true");
-            return optionsBuilder;
-        }
+        //private static DbContextOptionsBuilder<CertificateContext> BuildDbConnection()
+        //{
+        //    var optionsBuilder = new DbContextOptionsBuilder<CertificateContext>();
+        //    optionsBuilder.UseSqlServer(
+        //        "Server=(localdb)\\mssqllocaldb;Database=Certification;Trusted_Connection=True;MultipleActiveResultSets=true");
+        //    return optionsBuilder;
+        //}
 
-        [Test]
-        [Ignore("need to manually run")]
-        public void IsRepositoryInitializedWithValidNumberOfData()
-        {
-            var result = _repo.GetAll();
-            Assert.IsNotNull(result);
-            var numberOfRecords = result.ToList().Count;
-            Assert.AreEqual(6, numberOfRecords);
-        }
+        //[Test]
+        //[Ignore("need to manually run")]
+        //public void IsRepositoryInitializedWithValidNumberOfData()
+        //{
+        //    var result = _repo.GetAll();
+        //    Assert.IsNotNull(result);
+        //    var numberOfRecords = result.ToList().Count;
+        //    Assert.AreEqual(6, numberOfRecords);
+        //}
 
-        [Test]
-        [Ignore("manually run")]
-        public void Add_GivenTwoRecordsInDb_ShouldAddCertificateAndIncreaseRecordCount()
-        {
-            Certificate certificateToInsert = new Certificate
-            {
-                SequenceNumber = 1112,
-                IssueDate = DateTime.Now,
-                Document = null
-            };
-            _repo.Add(certificateToInsert);
-            var result = _repo.GetAll();
-            var numberOfRecords = result.ToList().Count;
-            Assert.AreEqual(6, numberOfRecords);
-        }
+        //[Test]
+        //[Ignore("manually run")]
+        //public void Add_GivenTwoRecordsInDb_ShouldAddCertificateAndIncreaseRecordCount()
+        //{
+        //    Certificate certificateToInsert = new Certificate
+        //    {
+        //        SequenceNumber = 1112,
+        //        IssueDate = DateTime.Now,
+        //        Document = null
+        //    };
+        //    _repo.Add(certificateToInsert);
+        //    var result = _repo.GetAll();
+        //    var numberOfRecords = result.ToList().Count;
+        //    Assert.AreEqual(6, numberOfRecords);
+        //}
 
     }
 }
