2753e7add92f8da8b39839f49fb662c8b5b00872
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-06 09:31:40 +0200

    chore: add todo's

--- CertificateProject.Repository/Domain/Certificate.cs
+++ CertificateProject.Repository/Domain/Certificate.cs
@@ -7,8 +7,10 @@ namespace CertificateProject.Core.Domain
     {
         [Key]
         public int Id { get; set; }
+        //TODO Add unique constraint
         public int SequenceNumber { get; set; }
         public DateTime IssueDate { get; set; }
+        //TODO add code to upload document at repository level
         public byte[] Document { get; set; }
     }
 }
